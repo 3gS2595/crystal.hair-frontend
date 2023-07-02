17921f414c979d8debeee4257fef91b6f898e8f6
trinkner <dbtrinkner@gmail.com>
2023-07-01 21:23:28 -0600

    cleaning

--- src/table/GetRss.js
+++ src/table/GetRss.js
@@ -1,5 +1,5 @@
-export const CustomerService = {
-  getCustomers (params) {
+export const ContentService = {
+  getContent (params) {
     return fetch('http://192.168.1.180:3000/rsses').then((res) =>
       res.json()
     )
